c98040d535fb0f5738005696d0bacf46d2de0570
rebolon <richard.tribes@gmail.com>
2015-08-06 16:52:05 +0200

    fix client

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'rebolon:socks5',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'This package allow to use Tor proxy. It uses almost same code provided by mattcg/socks5-http-client',
   git: 'https://github.com/Rebolon/meteor-package-http-socksclient.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('main.js');
+  api.addFiles(['main.js'], 'server');
   api.export(['socks'], 'server')
 });
 
