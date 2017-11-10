78dd4e43081d4d0d65b7e4b390dd1279acb55193
Rebolon <richard.tribes@gmail.com>
2017-11-10 22:36:18 +0100

    update summary

--- package.js
+++ package.js
@@ -1,13 +1,9 @@
 Package.describe({
   name: 'rebolon:socks5',
   version: '0.0.4',
-  summary: 'This package allow to use Tor proxy. It uses almost same code provided by mattcg/socks5-http-client',
+  summary: 'This package allow to use Tor proxy. It uses almost same code provided by mattcg/socks5-http-client and socks5-https-client',
   git: 'https://github.com/Rebolon/meteor-package-http-socksclient.git'
 });
-
-// @TODO : faire une app de base avec un HTTP.get sur du http et du https en passant par TOR
-// si c'est ok, on pousse le code et on modifie myalerts pour utiliser TOR
-// avec https il ne faut pas oublier de passer l'option strictSSL: true, au même titre que agentClass: Agent
 
 Package.onUse(function(api) {
   api.versionsFrom('1.6')
